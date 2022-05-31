fd3bbb79e3d4b0523df6423401c62c939b5eff9c
wgpdb <dariusz.borek@gmail.com>
2022-05-31 21:40:46 +0200

    Added CartController and CartDto - endpoint initial implementation

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.CartDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/carts")
 public class CartController {
+
+    @GetMapping(value = "{cartId}")
+    public CartDto getCart(@PathVariable Long cartId) {
+        return new CartDto(1L, "user 1");
+    }
+
+    @GetMapping
+    public List<CartDto> getCarts() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public void createCart(CartDto cartDto) {
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CartDto updateCart(CartDto cartDto) {
+        return new CartDto(1L, "edited user 1");
+    }
+
+    @DeleteMapping(value = "{cartId}")
+    public void deleteCart(@PathVariable Long cartId) {
+    }
 }
