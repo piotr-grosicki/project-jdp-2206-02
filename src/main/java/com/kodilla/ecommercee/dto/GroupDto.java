102573bb9072a06687a4521fdd10bba0b5a6e8b8
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-06-01 22:54:37 +0200

    Changed field productId to products

--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -3,11 +3,13 @@ package com.kodilla.ecommercee.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class GroupDto {
 
     private Long id;
-    private Long productId;
+    private List<String> products;
     private String name;
 }
