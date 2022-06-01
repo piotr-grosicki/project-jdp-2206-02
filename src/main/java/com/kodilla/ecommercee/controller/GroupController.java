f154355558f82eefa66bd0f62e5a2fd1e0cb54e4
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-06-01 16:27:19 +0200

    Initial endpoints for GroupController

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/groups")
 public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> getAllGroups() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{groupId}")
+    public GroupDto getGroup(@PathVariable Long groupId) {
+        return new GroupDto(1L, 1L, "Group");
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createGroup(@RequestBody GroupDto groupDto) {
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto(1L, 1L, "EditedGroup");
+    }
+
+    @DeleteMapping(value = "{groupId}")
+    public void deleteGroup(@PathVariable Long groupId) {
+    }
 }
