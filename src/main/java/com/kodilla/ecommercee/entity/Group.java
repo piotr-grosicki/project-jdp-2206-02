f40756712437c8a19b709343ba5be368e3d65fcd
Sebastian <sebaszymek@gmail.com>
2022-06-05 22:16:29 +0200

    Added Group Entity with commented relations and GroupRepository

---
+++ src/main/java/com/kodilla/ecommercee/entity/Group.java
@@ -0,0 +1,41 @@
+package com.kodilla.ecommercee.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "group_id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    /*
+    private List<Product> products;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Product> getProducts(){
+        return products;
+    }
+     */
+
+}
---
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommercee.repository;
+
+
+import com.kodilla.ecommercee.entity.Group;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GroupRepository extends CrudRepository<Group, Long> {
+
+    @Override
+    List<Group> findAll();
+
+    @Override
+    Group save(Group group);
+
+    @Override
+    Optional<Group> findById(Long id);
+
+}
