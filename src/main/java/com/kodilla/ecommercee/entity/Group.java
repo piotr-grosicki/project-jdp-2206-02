9a41c471057ec60ac14855436a5ff35306a727a8
Sebastian <sebaszymek@gmail.com>
2022-06-07 23:05:07 +0200

    Uncommented Group entity relations

--- src/main/java/com/kodilla/ecommercee/entity/Group.java
+++ src/main/java/com/kodilla/ecommercee/entity/Group.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -22,22 +23,20 @@ public class Group {
     @Column(name = "name")
     private String name;
 
-    public Group(String name) {
-        this.name = name;
-    }
-
-    /*
-    private List<Product> products;
-
     @OneToMany(
             targetEntity = Product.class,
             mappedBy = "group",
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY
     )
-    public List<Product> getProducts(){
-        return products;
+    private List<Product> products = new ArrayList<>();
+
+    public Group(String name, List<Product> products) {
+        this.name = name;
+        this.products = products;
     }
-     */
+
+
+
 
 }
