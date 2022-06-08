76b9bae3fd9d802021fbde588f37fcd469a5f18c
Tomasz Marciniak <huron3572@gmail.com>
2022-06-08 18:53:12 +0200

    Added relationships to user entity

--- src/main/java/com/kodilla/ecommercee/entity/User.java
+++ src/main/java/com/kodilla/ecommercee/entity/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -17,38 +18,44 @@ public final class User {
     @Column(name = "userId", unique = true)
     private Long id;
 
+    @NotNull
     @Column(name = "name")
     private String name;
 
+    @NotNull
     @Column(name = "surname")
     private String surname;
 
+    @NotNull
     @Column(name = "status")
     private boolean status;
 
+    @NotNull
     @Column(name = "userKey")
     private int userKey;
+
+    @OneToMany(
+            targetEntity = Cart.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private List<Cart> carts;
+
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "userId",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private List<Order> orders;
 
     public User(String name, String surname, boolean status, int userKey){
         this.name = name;
         this.surname = surname;
         this.status = status;
-        this.userKey = userKey;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setSurname(String surname){
-        this.surname= surname;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public void setUserKey(int userKey) {
         this.userKey = userKey;
     }
 }
