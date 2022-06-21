b34eaf1a571bab1de8758c373a2fe4763e994b3b
wgpdb <dariusz.borek@gmail.com>
2022-06-21 14:27:58 +0200

    Updated OrderMapper & OrderController

--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -2,10 +2,10 @@ package com.kodilla.ecommercee.mapper;
 
 import com.kodilla.ecommercee.dto.OrderDto;
 import com.kodilla.ecommercee.entity.Order;
+import com.kodilla.ecommercee.exception.CartNotFoundException;
 import com.kodilla.ecommercee.exception.UserNotFoundException;
-import com.kodilla.ecommercee.repository.CartRepository;
+import com.kodilla.ecommercee.service.CartService;
 import com.kodilla.ecommercee.service.UserDbService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,15 +15,13 @@ import java.util.stream.Collectors;
 public class OrderMapper {
 
     UserDbService userDbService;
+    CartService cartDbService;
 
-    @Autowired
-    CartRepository cartRepository;
-
-    public Order mapToOrder(final OrderDto orderDto) throws UserNotFoundException {
+    public Order mapToOrder(final OrderDto orderDto) throws UserNotFoundException, CartNotFoundException {
         return Order.builder()
                 .id(orderDto.getOrderId())
                 .user(userDbService.getUser(orderDto.getUserId()))
-                .cart(cartRepository.findById(orderDto.getCartId()).get())
+                .cart(cartDbService.getNewCart(orderDto.getCartId()))
                 .created(orderDto.getCreated())
                 .orderStatus(orderDto.getOrderStatus())
                 .build();
